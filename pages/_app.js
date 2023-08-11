b514f5e7395684fe9a2e07d16576807e6e306014
ParadigmaRama <ramirogimenezcontacto@gmail.com>
2023-08-11 12:41:02 -0300

    new changes

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import Header from "@/components/header/header"
+import "./globals.css"
+
+export default function App(){
+	return(
+		<div>
+			<Header />
+			<main>
+				contenido
+			</main>
+		</div>
+	)
+}
